ae04bbaa7836f806e8d73c7426e065c36144ab05
bamcneely <bamcneely14@gmail.com>
2017-12-02 15:32:09 -0700

    Formatted

--- sevensegment_controller.vhd
+++ sevensegment_controller.vhd
@@ -48,8 +48,8 @@ end component;
 
 component khz_clk is
     port( clk_in : in  std_logic;
-           reset  : in  std_logic;
-           clk_out: out std_logic
+          reset  : in  std_logic;
+          clk_out: out std_logic
         );
 end component;
 
