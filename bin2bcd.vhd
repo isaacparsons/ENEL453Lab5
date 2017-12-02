da0e5020f60bc87b8a85de382c3b7a9dfcc032e4
bamcneely <bamcneely14@gmail.com>
2017-12-02 15:43:00 -0700

    Update bin2bcd.vhd

--- bin2bcd.vhd
+++ bin2bcd.vhd
@@ -5,12 +5,12 @@ use ieee.numeric_std.all;
 entity bin2bcd is
     port ( clk : in std_logic;                                      
            rst : in std_logic;
-           acd_bin_out : in  std_logic_vector (10 downto 0);   -- adc output value as a binary string              
-           cm_tens : out std_logic_vector (3 downto 0);            -- needs to display 0-9
-           cm_ones : out std_logic_vector (3 downto 0);            -- needs to display 0-9
-           cm_tenths : out std_logic_vector (3 downto 0);          -- needs to display 0-9
-           cm_hundredths : out std_logic_vector (3 downto 0)       -- needs to display 0-9
-          );
+           acd_bin_out : in  std_logic_vector (10 downto 0);	-- adc output value as a binary string              
+           cm_tens : out std_logic_vector (3 downto 0);         -- can display 0-9
+           cm_ones : out std_logic_vector (3 downto 0);         -- can display 0-99
+           cm_tenths : out std_logic_vector (3 downto 0);       -- can display 0-9
+           cm_hundredths : out std_logic_vector (3 downto 0)    -- can display 0-9
+         );
           
 end bin2bcd;
 
@@ -18,6 +18,7 @@ architecture behavioral of bin2bcd is
 begin
 
 bcd1: process(clk, rst)
+	
 variable temp : std_logic_vector (10 downto 0); -- temporary variable
 variable bcd : unsigned (15 downto 0) := (others => '0'); -- variable to store the output bcd number
 
@@ -46,13 +47,14 @@ begin
             bcd(11 downto 8) := bcd(11 downto 8) + 3;
           end if; 
 
-		  if bcd(15 downto 12) > 4 then  
+	  if bcd(15 downto 12) > 4 then  
             bcd(15 downto 12) := bcd(15 downto 12) + 3;
           end if;		  
           
           bcd := bcd(14 downto 0) & temp(10); -- shift bcd left by 1 bit, copy msb of temp into lsb of bcd       
           temp := temp(9 downto 0) & '0'; -- shift temp left by 1 bit    
-        end loop;     
+        end loop;    
+
         -- set outputs
         cm_tens <= std_logic_vector(bcd(15 downto 12));
         cm_ones <= std_logic_vector(bcd(11 downto 8));
